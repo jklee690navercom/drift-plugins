be7ff69c04c3c92b624536d79d7d023baf8894ba
jklee690 <jklee@surromind.ai>
2026-03-24 14:10:19 +0900

    Delete plugin: _harness_ver_test

--- plugins/_harness_ver_test/drift__harness_ver_test/__init__.py
+++
@@ -1,9 +0,0 @@
-def register(app):
-    from framework.plugin.types import PluginInfo
-    from framework.plugin.base import DriftDetector
-    class TestDetector(DriftDetector):
-        def detect(self, data, data_ids, stream, params): return []
-    return PluginInfo(
-        key='_harness_ver_test', name='Test Plugin', version='1.0.0',
-        description='Harness test plugin', category='statistical',
-        card_template='', page_url='', detector_class=TestDetector)
